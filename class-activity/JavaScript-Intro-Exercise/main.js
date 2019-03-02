aec3f7cd8488c5e5bd656a09f6cf6465bce6c09e
luw779 <luw779@newschool.edu>
2019-03-02 17:04:39 -0500

    update

---
+++ class-activity/JavaScript-Intro-Exercise/main.js
@@ -0,0 +1,106 @@
+var myName="Cecilia";
+console.log(myName);
+
+var number="3";
+console.log(number);
+
+var favNumber="three"
+console.log(favNumber);
+
+var t= true;
+console.log(t)
+
+var name= ["C", "E", "C", "I", "L", "I", "A"];
+console.log(name)
+
+var action= ["run", "jump", "walk"];
+console.log(action)
+
+var lastName ="Lu"
+var fullName = ["Cecilia" + " " + "Lu"];
+console.log(firstName)
+
+var addNumber= 45+55;
+console.log(addNumber);
+
+var name = name[0]; 
+var name = name[1];  
+var name = name[2]; 
+var name = name[3];
+var name = name[4]; 
+console.log(name);
+
+var str = "supercalifragilisticexpialidocious";
+var n = str.length;
+console.log(n);
+
+//#5
+var firstandlast= [myName.length == lastName.length]
+console.log(firstandlast)
+
+var students= 14
+if(students>16){
+	var areHere="Everyone is here";
+}
+else{
+	var notHere="Not everyone is here";
+}
+console.log()
+//not sure how to do #5
+
+//#6
+
+
+//#7
+var date= new Date;
+var hour= now.getHours();
+if 
+
+//#8
+var day =date.getDay();
+if (day==1){
+	console.log('Monday');
+}
+
+else if (day==2){
+	console.log('Tuesday');
+}
+
+else if (day==3){
+	console.log('Wednesday');
+}
+
+else if (day==4){
+	console.log('Thursday');
+}
+
+else if (day==5){
+	console.log('Friday');
+}
+
+else if (day==6){
+	console.log('Saturday');
+}
+
+else if (day==7){
+	console.log('Sunday');
+}
+
+//#9
+var food= ["Raman", "Sushi", "KoreanBBQ"];
+console.log(food)
+
+var animals= ["Dog", "Panda", "Bird"];
+console.log(animals)
+
+var places= ["Myhome", "Japan", "Taiwan"];
+console.log(places)
+
+var pokemon= ["Pikachu", "Squirtle", "Raichu"];
+console.log(pokemon)
+
+//#10
+var array= [food, animals, places, pokemon];
+console.log(array)
+
+var nested=
